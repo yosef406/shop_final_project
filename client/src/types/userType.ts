fe767ca10e15dcb234a96f4fe9c328062c6a1acd
Yosef Awad <yosef.awad.1@gmail.com>
2022-11-12 09:03:15 +0200

    feat: added user type

---
+++ client/src/types/userType.ts
@@ -0,0 +1,12 @@
+export default interface userType {
+    _id: string,
+    id: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string,
+    city: string,
+    street: string,
+    role: string,
+    signedIn: boolean
+}
