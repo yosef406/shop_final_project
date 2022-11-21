51d4c937d08b5eae4301a4249be901814f4f5eb6
Yosef Awad <yosef.awad.1@gmail.com>
2022-11-21 16:52:31 +0200

    feat: added button component

---
+++ client/src/components/button/Button.tsx
@@ -0,0 +1,44 @@
+import { useRef } from "react";
+import Loading from "../loading/Loading";
+import style from "./button.module.scss";
+export default function Button(params: any) {
+  const btnRef = useRef<HTMLButtonElement | null>(null);
+
+  const playDisabledAnimations = () => {
+    btnRef.current?.classList.add(style.animateDisabled);
+    setTimeout(() => {
+      btnRef.current?.classList.remove(style.animateDisabled);
+    }, 400);
+  };
+
+  return (
+    <>
+      <button
+        ref={btnRef}
+        className={`
+        ${
+          params?.disabled
+            ? style.disabled
+            : params?.loading
+            ? style.loading
+            : style.active
+        } ${style.row}
+        `}
+        onClick={params?.disabled ? playDisabledAnimations : params.onClick}
+      >
+        {
+          <>
+            {params?.loading && !params?.disabled ? (
+              <span>
+                <Loading size={1} />
+              </span>
+            ) : (
+              ""
+            )}
+            {params.children}
+          </>
+        }
+      </button>
+    </>
+  );
+}
