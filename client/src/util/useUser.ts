1fee76b21e9dfff4b6b35e417355745cd6a9eb84
Yosef Awad <yosef.awad.1@gmail.com>
2022-11-21 16:51:09 +0200

    feat: user hook

---
+++ client/src/util/useUser.ts
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getUserSignInState, getUserRole, getUser, addUser as addUserAction, removeUser as removeUserAction } from "../data/slices/userSlice";
+import userType from "../types/userType";
+
+export default function useUser() {
+    const dispatch = useDispatch();
+    const user = useSelector(getUser);
+    const role = useSelector(getUserRole);
+    const state = useSelector(getUserSignInState);
+
+    const addUser = (user: userType) =>
+        dispatch(addUserAction(user));
+
+    const removeUser = () => dispatch(removeUserAction());
+
+    return { user, role, state, addUser, removeUser };
+}
