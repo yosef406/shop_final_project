800835f2b49699fd61923ce5e570522dc2a2215c
Yosef Awad <yosef.awad.1@gmail.com>
2022-11-27 12:44:14 +0200

    feat: added a log out button

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,5 +1,8 @@
+import useUser from "../../util/useUser";
+import Button from "../button/Button";
 import style from "./header.module.scss";
 function Header() {
+  const { user, removeUser } = useUser();
   return (
     <>
       <div className={style.container}>
@@ -14,10 +17,18 @@ function Header() {
                 <span> Support@Shopi.co</span>
               </h3>
             </div>
-            {/* <div className={style.user}>
-              <label>Welcome user</label>
-              <button>Log Out</button>
-            </div> */}
+            {user.signedIn ? (
+              <div className={style.user}>
+                <label>
+                  Welcome {user.first_name} {user.last_name}
+                </label>
+                <Button className={style.logOutBtn} onClick={removeUser}>
+                  Log out
+                </Button>
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </div>
