62387d80863550eb55aed1f84017a1c4161b8606
Yosef Awad <yosef.awad.1@gmail.com>
2022-11-27 17:03:16 +0200

    style updated product max size

---
+++ client/src/components/product/Product.tsx
@@ -0,0 +1,45 @@
+import productType from "../../types/productType";
+import Button from "../button/Button";
+import Card from "../card/Card";
+import style from "./product.module.scss";
+
+export default function Product(params: { value?: productType | undefined }) {
+  const product = params.value ?? null;
+  return (
+    <>
+      {product != null ? (
+        <>
+          <Card className={style.cardStyle}>
+            <div className={style.container}>
+              <img
+                src={product?.image}
+                alt="product"
+                className={style.productImage}
+              />
+              <h3>{product?.name}</h3>
+              <label>{product?.category}</label>
+              <h3>{product?.price} $</h3>
+              <div className={style.productButton}>
+                <Button onClick={() => {}}>Add</Button>
+              </div>
+            </div>
+          </Card>
+        </>
+      ) : (
+        <>
+          <Card className={style.cardStyle}>
+            <div className={style.container}>
+              <div className={style.skeletonImage}></div>
+              <div className={style.skeletonTextL}></div>
+              <div className={style.skeletonTextS}></div>
+              <div className={style.skeletonTextL}></div>
+              <div className={style.productButton}>
+                <Button loading={true}>Add</Button>
+              </div>
+            </div>
+          </Card>
+        </>
+      )}
+    </>
+  );
+}
