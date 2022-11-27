18122df5a3ac7faa0728fc79fad082c4c9c3b540
Yosef Awad <yosef.awad.1@gmail.com>
2022-11-27 14:31:03 +0200

    feat: added categories redux

---
+++ client/src/data/slices/categorySlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+import categoryType from '../../types/categoryType';
+
+export const categorySlice = createSlice({
+
+    name: 'category',
+    initialState: {
+        value: [{
+            _id: "",
+            name: "",
+        }]
+    },
+
+    reducers: {
+        addCategories: (state, action) => {
+            state.value = action.payload;
+        },
+
+        removeCategories: (state) => {
+            state.value = [{
+                _id: "",
+                name: "",
+            }]
+        }
+    },
+});
+
+export const { addCategories, removeCategories } = categorySlice.actions;
+
+export const getCategories = (state: { categories: { value: categoryType } }) => state.categories.value;
+
+
+export default categorySlice.reducer;
