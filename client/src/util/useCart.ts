7e2475eb14fc681e3cab8d04ba0be5ced48732a3
Yosef Awad <yosef.awad.1@gmail.com>
2022-11-28 08:56:01 +0200

    feat: added function to count cart items

--- client/src/util/useCart.ts
+++ client/src/util/useCart.ts
@@ -14,9 +14,21 @@ export default function useCart() {
     const cart = useSelector(getCart);
 
     const addCart = (cart: cartType) => dispatch(addCartAction(cart));
+    const getCartCounted = () => {
+        const count: any = {};
+        for (const prod of cart.products) {
+            count[prod] = count[prod] ? count[prod] + 1 : 1;
+        }
+        let objCount: [{ _id: string, num: number }] | Array<{ _id: string, num: number }> = [];
+        for (const _id in count) {
+            objCount.push({ _id, num: count[_id] });
+        }
+        return objCount;
+    }
+    const productCount = getCartCounted();
     const removeCart = () => dispatch(removeCartAction());
     const addProduct = (product: string) => dispatch(addProductAction(product))
     const removeProduct = (product: string) => dispatch(removeProductAction(product))
 
-    return { cart, addCart, removeCart, addProduct, removeProduct };
+    return { cart, addCart, removeCart, addProduct, removeProduct, productCount };
 }
