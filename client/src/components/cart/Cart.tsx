b3e225de7bb0a2061afcebedfee2e8fb2369b49c
Yosef Awad <yosef.awad.1@gmail.com>
2022-11-28 23:24:33 +0200

    feat: cart

--- client/src/components/cart/Cart.tsx
+++ client/src/components/cart/Cart.tsx
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useCart from "../../util/useCart";
+import useFetch from "../../util/useFetch";
+import useProduct from "../../util/useProduct";
 import useUser from "../../util/useUser";
 import Button from "../button/Button";
 import Card from "../card/Card";
+import CartItem from "../cartItem/CartItem";
+import CheckOut from "../checkOut/CheckOut";
 import NewProduct from "../newProduc/NewProduct";
 import PopUp from "../popUp/PopUp";
 import style from "./cart.module.scss";
 
 export default function Cart() {
-  const { role } = useUser();
+  const { role, user } = useUser();
+  const { productCount, addCart, cart } = useCart();
   const [popUp, setPopUp] = useState(false);
+  const [checkOut, setCheckOut] = useState(false);
+  const { data, loading, request } = useFetch();
 
+  useEffect(() => {
+    if (cart._id === "" && role !== "admin") {
+      request.get("/cart/open/" + user._id);
+    }
+  });
+
+  useEffect(() => {
+    if (data && data.message === "found open cart") {
+      addCart(data.cart);
+    }
+  }, [data]);
   return (
     <>
       {role === "admin" ? (
@@ -28,12 +47,40 @@ export default function Cart() {
                   Add Product
                 </Button>
               </div>
-              <h1>admin</h1>
+              <h1>admin panel</h1>
             </Card>
           </div>
         </>
       ) : (
-        ""
+        <>
+          <PopUp setDisplay={setCheckOut} display={checkOut}>
+            <CheckOut />
+          </PopUp>
+          <div className={style.container}>
+            <Card className={style.cart}>
+              <h1>Cart</h1>
+              {productCount.map((val) => (
+                <CartItem
+                  key={val._id}
+                  disabled={loading}
+                  val={val}
+                  deletable={true}
+                />
+              ))}
+              <div>
+                <h2>total price: {cart.total_price} $</h2>
+                <Button
+                  onClick={() => {
+                    setCheckOut(true);
+                  }}
+                  loading={loading}
+                >
+                  Check Out
+                </Button>
+              </div>
+            </Card>
+          </div>
+        </>
       )}
     </>
   );
